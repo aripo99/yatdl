a150d46f07e17024b6ee81b1065da1f4c55e9bb7
aripo99 <a_polakof@umail.ucsb.edu>
2023-11-19 01:24:26 -0800

    Fix sidebar styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { SidebarNav } from '@/components/sidebar-nav'
+import { Separator } from '@/components/ui/separator'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -35,6 +36,13 @@ export default function SettingsLayout({ children }: SettingsLayoutProps) {
     <html lang="en">
       <body className={inter.className}>
         <div className="space-y-6 p-10 pb-16 md:block">
+          <div className="space-y-0.5">
+            <h2 className="text-2xl font-bold tracking-tight">YATDL</h2>
+            <p className="text-muted-foreground">
+              Yet Another To Do List
+            </p>
+          </div>
+          <Separator className="my-6" />
           <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
             <aside className="-mx-4 lg:w-1/5">
               <SidebarNav items={sidebarNavItems} />
--- components/sidebar-nav.tsx
+++ components/sidebar-nav.tsx
@@ -2,8 +2,28 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-
+import { cva } from "class-variance-authority"
 import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+    "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+    {
+        variants: {
+            variant: {
+                ghost: "hover:bg-accent hover:text-accent-foreground",
+            },
+            size: {
+                default: "h-9 px-4 py-2",
+                sm: "h-8 rounded-md px-3 text-xs",
+                lg: "h-10 rounded-md px-8",
+                icon: "h-9 w-9",
+            },
+        },
+        defaultVariants: {
+            size: "default",
+        },
+    }
+)
 
 interface SidebarNavProps extends React.HTMLAttributes<HTMLElement> {
     items: {
@@ -28,7 +48,7 @@ export function SidebarNav({ className, items, ...props }: SidebarNavProps) {
                     key={item.href}
                     href={item.href}
                     className={cn(
-                        "hover:bg-accent hover:text-accent-foreground",
+                        buttonVariants({ variant: "ghost" }),
                         pathname === item.href
                             ? "bg-muted hover:bg-muted"
                             : "hover:bg-transparent hover:underline",
