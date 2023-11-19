c4c5e6724dd80d2dd34f5381365099c7bb247132
aripo99 <a_polakof@umail.ucsb.edu>
2023-11-19 01:35:21 -0800

    Create to do list skeleton components

---
+++ components/to-do-list.tsx
@@ -0,0 +1,11 @@
+import { ToDo } from './to-do';
+
+export function ToDoList() {
+    return (
+        <div>
+            <ToDo />
+            <ToDo />
+            <ToDo />
+        </div>
+    )
+}
---
+++ components/to-do.tsx
@@ -0,0 +1,7 @@
+export function ToDo() {
+    return (
+        <div>
+            <h1>To Do</h1>
+        </div>
+    )
+}
