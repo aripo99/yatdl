c06f60e1227ef7a9cbf5c642c407665eb4326fea
aripo99 <a_polakof@umail.ucsb.edu>
2023-11-19 01:38:49 -0800

    Create all main pages

---
+++ app/backlog/page.tsx
@@ -0,0 +1,9 @@
+import { ToDoList } from "@/components/to-do-list"
+
+export default function Home() {
+    return (
+        <main className="flex">
+            <ToDoList />
+        </main>
+    )
+}
