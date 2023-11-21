a961929ba02e30e73edeabe3d582057ee2e6e996
aripo99 <a_polakof@umail.ucsb.edu>
2023-11-20 19:37:39 -0800

    Update to do list component to show todos

--- components/to-do-list.tsx
+++ components/to-do-list.tsx
@@ -1,12 +1,27 @@
 import { ToDo } from './to-do';
 import { RadioGroup } from "@/components/ui/radio-group"
+import getSupabaseBrowserClient from "@/lib/supabase/server-client"
 
-export function ToDoList() {
+export async function ToDoList() {
+    const todos = await fetchToDos();
+
+    async function fetchToDos() {
+        "use server";
+        const client = getSupabaseBrowserClient();
+        const sessionResponse = await client.auth.getSession();
+        const user = sessionResponse.data?.session?.user;
+        const { data: todos, error } = await client.from("todos").select().eq("user_id", user?.id);
+        if (error) {
+            console.error(error);
+        }
+        return todos;
+    }
+
     return (
         <RadioGroup>
-            <ToDo id="r1" />
-            <ToDo id="r2" />
-            <ToDo id="r3" />
+            {todos && todos.map((todo) => (
+                <ToDo key={todo.id} id={todo.id} title={todo.title} />
+            ))}
         </RadioGroup>
     )
 }
--- components/to-do.tsx
+++ components/to-do.tsx
@@ -3,13 +3,14 @@ import { RadioGroupItem } from "@/components/ui/radio-group"
 
 interface ToDoProps {
     id: string;
+    title: string;
 }
 
-export function ToDo({ id }: ToDoProps) {
+export function ToDo({ id, title }: ToDoProps) {
     return (
         <div className="flex items-center space-x-2">
             <RadioGroupItem value={id} id={id} />
-            <Label htmlFor={id}>To Do {id}</Label>
+            <Label htmlFor={id}> {title} </Label>
         </div>
     )
 }
