b1d066d3ce38432806b760eaa385e126b86ec71f
aripo99 <a_polakof@umail.ucsb.edu>
2023-11-24 01:00:43 -0800

    Reset todo title when adding a todo

--- components/add-to-do.tsx
+++ components/add-to-do.tsx
@@ -1,17 +1,27 @@
+"use client";
+
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
-import addToDo from "../lib/actions/add-to-do";
+import addToDo from "../lib/actions/add-to-do"
+import { useState } from "react"
 
 interface AddToDoProps {
     path: string;
 }
 
 export default function AddToDo(props: AddToDoProps) {
+    const [title, setTitle] = useState('');
+
+    const handleAddClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        await addToDo(title, props.path);
+        setTitle('');
+    }
+
     return (
-        <form action={addToDo} className="mt-4 flex flex-row">
-            <input type='hidden' name='path' value={props.path} />
-            <Input type="text" name="title" className="rounded-md p-2" placeholder="Add a new todo" />
-            <Button className="ml-2">Add</Button>
-        </form>
+        <>
+            <Input type="text" name="title" className="rounded-md p-2" placeholder="Add a new todo" value={title} onChange={(e) => setTitle(e.target.value)} />
+            <Button className="mt-2" onClick={handleAddClick}>Add</Button>
+        </>
     )
 }
--- lib/actions/add-to-do.ts
+++ lib/actions/add-to-do.ts
@@ -3,12 +3,10 @@
 import getSupabaseServerClient from "@/lib/supabase/server-client"
 import { revalidatePath } from 'next/cache';
 
-export default async function addToDo(formData: FormData) {
+export default async function addToDo(title: string, path: string) {
     const client = getSupabaseServerClient();
     const sessionResponse = await client.auth.getSession();
     const user = sessionResponse.data?.session?.user;
-    const title = formData.get("title") as string;
-    const path = formData.get("path") as string;
     const { error } = await client.from("todos").insert([{ title, user_id: user?.id, category: path.split("/")[1] }]);
     if (error) {
         console.error(error);
