b447bc38032bd1480f252e9cab17cc75a646e471
aripo99 <a_polakof@umail.ucsb.edu>
2023-12-04 21:47:56 -0800

    Add separator

--- app/(app)/backlog/page.tsx
+++ app/(app)/backlog/page.tsx
@@ -1,9 +1,11 @@
 import { ToDoList } from "@/components/to-do-list"
 import AddToDo from "@/components/add-to-do"
+import { Separator } from "@/components/ui/separator"
 
 export default function Home() {
     return (
         <div>
+            <Separator className="my-8" />
             <p className="mb-6 text-lg">Backlog</p>
             <AddToDo path="/backlog" />
             <ToDoList path="/backlog" />
--- app/(app)/today/page.tsx
+++ app/(app)/today/page.tsx
@@ -1,13 +1,16 @@
 import { ToDoList } from "@/components/to-do-list"
 import AddToDo from "@/components/add-to-do"
 import CompletedToDos from "@/components/completed-to-dos"
+import { Separator } from "@/components/ui/separator"
 
 export default function Home() {
     return (
         <div>
+            <Separator className="my-8" />
             <p className="mb-6 text-3xl">Today&apos;s To Do&apos;s</p>
             <AddToDo path="/today" />
             <ToDoList path="/today" />
+            <Separator className="my-8" />
             <CompletedToDos />
         </div>
     )
