9e080e8e328e1f9a1591504a7fec91eb5c8772e8
aripo99 <a_polakof@umail.ucsb.edu>
2023-12-11 20:35:24 -0800

    Add constraint in labels, needed for trigger

--- supabase/migrations/20231120061146_schema.sql
+++ supabase/migrations/20231120061146_schema.sql
@@ -39,6 +39,7 @@ create table todos (
     title text,
     category text CHECK (category IN ('today', 'backlog')),
     is_complete boolean default false,
+    labels text[],
     created_at timestamptz not null default now(),
     updated_at timestamptz not null default now()
 );
--- supabase/migrations/20231212033003_schema.sql
+++ supabase/migrations/20231212033003_schema.sql
@@ -3,8 +3,11 @@ create table labels (
     uuid uuid not null unique default gen_random_uuid(),
     user_id uuid not null references auth.users on delete cascade,
     name text,
-    created_at timestamptz not null default now()
+    created_at timestamptz not null default now(),
+    unique (name, user_id)
 );
+
+alter table labels add constraint unique_label_name_per_user unique (name, user_id);
 
 alter table labels enable row level security;
 
@@ -28,3 +31,35 @@ todo_labels
     with check (auth.uid () = todo_labels.user_id);
 
 alter table todo_labels enable row level security;
+
+create function public.handle_new_todo()
+returns trigger 
+language plpgsql 
+security definer set search_path = public
+as $$
+declare
+  label_name text;
+  label_id bigint;
+begin
+  -- Iterate over each label in the labels array
+  foreach label_name in array new.labels
+  loop
+    -- Insert label if it doesn't exist and get its ID
+    insert into labels (name, user_id)
+    values (label_name, new.user_id)
+    on conflict (name, user_id) do nothing;
+
+    select id into label_id from labels where name = label_name and user_id = new.user_id;
+
+    -- Link the label to the todo
+    insert into todo_labels (todo_id, label_id, user_id)
+    values (new.id, label_id, new.user_id);
+  end loop;
+
+  return new;
+end;
+$$;
+
+create trigger on_todo_created
+  after insert on public.todos
+  for each row execute function public.handle_new_todo();
